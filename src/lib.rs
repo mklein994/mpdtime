bc2e674c310f604dbd6319bb33edb41821000ceb
Matthew Klein <mklein994@gmail.com>
2018-02-17 13:26:07 -0600

    Handle client connection error and missing time error in main()

--- src/lib.rs
+++ src/lib.rs
@@ -40,29 +40,20 @@ impl From<mpd::error::Error> for Error {
 type Result<T> = std::result::Result<T, Error>;
 
 pub fn run() -> Result<()> {
-    let mut conn = match Client::connect("127.0.0.1:6600") {
-        Ok(c) => c,
-        Err(_) => {
-            eprintln!("Could not connect to mpd");
-            std::process::exit(1);
-        }
-    };
+    let mut conn = Client::connect("127.0.0.1:6600")?;
 
     let status = conn.status()?;
 
-    let (elapsed, total) = match status.time {
-        Some(t) => t,
-        None => std::process::exit(0),
-    };
-
-    print!(
-        "{}:{:02}/{}:{:02} ({}%)",
-        elapsed.num_minutes(),
-        elapsed.num_seconds() - (elapsed.num_minutes() * 60),
-        total.num_minutes(),
-        total.num_seconds() - (total.num_minutes() * 60),
-        (elapsed.num_seconds() as f64 / total.num_seconds() as f64 * 100.0).trunc()
-    );
+    if let Some((elapsed, total)) = status.time {
+        print!(
+            "{}:{:02}/{}:{:02} ({}%)",
+            elapsed.num_minutes(),
+            elapsed.num_seconds() - (elapsed.num_minutes() * 60),
+            total.num_minutes(),
+            total.num_seconds() - (total.num_minutes() * 60),
+            (elapsed.num_seconds() as f64 / total.num_seconds() as f64 * 100.0).trunc()
+        );
+    }
 
     Ok(())
 }
